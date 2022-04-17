531658c5667626bb63718afa31a8a4c2d9d44b47
foderking <freatshow@gmail.com>
2022-04-17 22:18:51 +0100

    done UI for right sidebar / moves bar, also added icon to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,37 +3,37 @@
 
 export default function Navbar() {
 	return(
-	<nav id='nav' className="navbar navbar-expand-lg navbar-dark ">
-		<button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-			data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
-			aria-label="Toggle navigation">
-			<span className="navbar-toggler-icon"></span>
-		</button>
-		<a className="navbar-brand" href="#!">
-			<img src="https://i.imgur.com/ky3AW06.png" width="30" height="30" className="d-inline-block align-top" alt=""></img>
-			{/* Light-Chess */}
-		</a>
+		<nav id='nav' className="navbar navbar-expand-lg navbar-dark ">
+			<button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+				data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<a className="navbar-brand" href="#!">
+				<img src="https://i.imgur.com/ky3AW06.png" width="30" height="30" className="d-inline-block align-top" alt=""></img>
+				{/* Light-Chess */}
+			</a>
 
-		<div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-			<ul className="navbar-nav mr-auto mt-2 mt-md-0">
-				<li className="nav-item active">
-					<a className="nav-link" href="#!">Home
-						{/* <span className="sr-only">(current)</span> */}
-					</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#!">Link</a>
-				</li>
-				<li className="nav-item">
-					<a className="nav-link" href="#!">Disabled</a>
-				</li>
-			</ul>
-			{/* <span classNameName='float-right'>
-				<form className="form-inline my-2 my-lg-0">
-					<input className="form-control mr-sm-2" type="text" placeholder="Search" />
-				</form>
-			</span> */}
-		</div>
-	</nav>
+			<div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+				<ul className="navbar-nav mr-auto mt-2 mt-md-0">
+					<li className="nav-item active">
+						<a className="nav-link" href="#!">Home
+							{/* <span className="sr-only">(current)</span> */}
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#!">Link</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#!">Disabled</a>
+					</li>
+				</ul>
+				{/* <span classNameName='float-right'>
+					<form className="form-inline my-2 my-lg-0">
+						<input className="form-control mr-sm-2" type="text" placeholder="Search" />
+					</form>
+				</span> */}
+			</div>
+		</nav>
 	)
  }
--- src/Components/RightSidebar.js
+++ src/Components/RightSidebar.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
-export default function RightSidebar(){
+export default function RightSidebar({history}){
 	return (
-		<div className='right-sidebar text-center'>
-			<h3>
-				Right Sidebar
-			</h3>
+		<div className='right-sidebar analyze text-center'>
+			<div className='ceval'>
+				<span className='pearl'></span>
+				<div className='help'>
+					<span>Random Title</span>
+				</div>
+				<div className='switch'></div>
+			</div>
+			
+			<div className='analyze-moves'>
+				{/* <div class="result">0-1</div> */}
+
+				<div className='tview2 tview2-column'>
+					{
+						history.map( (each,i) =>
+						<>
+							{
+								i % 2
+								?
+								<div className='move'><span>{each}</span></div>
+								:
+								<>
+									<div className='index'>{parseInt(i/2)+1}</div>
+									<div className='move'><span>{each}</span></div>
+								</>
+							}
+						</>
+						)
+					}
+				</div>
+				<div class="status">White resigned • Black is victorious</div>
+
+			</div>
 		</div>
 	)
 }
